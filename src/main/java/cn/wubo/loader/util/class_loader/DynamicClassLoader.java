61759104732f44eee6e17097b192ddc75ee457fe
dh-free <hao.ding@insentek.com>
2024-02-21 10:45:21 +0800

    扩展实现能够处理和收集编译后的所有类（包括内部类）

--- src/main/java/cn/wubo/loader/util/class_loader/DynamicClassLoader.java
+++ src/main/java/cn/wubo/loader/util/class_loader/DynamicClassLoader.java
@@ -1,6 +1,8 @@
 package cn.wubo.loader.util.class_loader;
 
 import java.security.SecureClassLoader;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @description:动态编译加载器
@@ -10,25 +12,24 @@ import java.security.SecureClassLoader;
 public class DynamicClassLoader extends SecureClassLoader {
 
     /**
-     * 编译的时候返回的class字节数组
+     * 编译的时候返回的class字节数组-支持内部类
      */
-    private byte[] classData;
+    private final Map<String, byte[]> classBytes = new HashMap<>();
 
-    public DynamicClassLoader(byte[] classData) {
-        super();
-        this.classData = classData;
+    public DynamicClassLoader(ClassLoader parent) {
+        super(parent);
+    }
+
+    public void addClass(String fullClassName, byte[] classData) {
+        classBytes.put(fullClassName, classData);
     }
 
     @Override
     protected Class<?> findClass(String fullClassName) throws ClassNotFoundException {
-        // 1. 判断编译的class字节数组为null，若为null则说明已经编译过，无需再编译
-        if (classData == null || classData.length == 0)
-            throw new ClassNotFoundException("[动态编译]classdata不存在");
-        // 2. 加载class字节数组
+        byte[] classData = classBytes.get(fullClassName);
+        if (classData == null) {
+            throw new ClassNotFoundException("[动态编译]找不到类: " + fullClassName);
+        }
         return defineClass(fullClassName, classData, 0, classData.length);
     }
-
-
-
-
 }
