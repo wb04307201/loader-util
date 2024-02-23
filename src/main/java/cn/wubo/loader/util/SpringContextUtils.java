ac4c5ad4bf326eb1edc91e631162ea75eff5cc17
吴博 <wubo_aaa@163.com>
2024-02-23 12:37:07 +0800

    注册单例Bean前自动装配Bean属性

--- src/main/java/cn/wubo/loader/util/SpringContextUtils.java
+++ src/main/java/cn/wubo/loader/util/SpringContextUtils.java
@@ -18,22 +18,44 @@ public class SpringContextUtils implements BeanFactoryAware {
         listableBeanFactory = (DefaultListableBeanFactory) beanFactory;
     }
 
+    /**
+     * 注册单例Bean
+     * @param type Bean的类型
+     */
     public static <T> void registerSingleton(Class<T> type) {
+        // 创建Bean实例
         T obj = listableBeanFactory.createBean(type);
+        // 获取Bean名称
         String beanName = beanName(type.getName());
+        // 自动装配Bean属性
+        listableBeanFactory.autowireBean(obj);
+        // 注册单例Bean
         listableBeanFactory.registerSingleton(beanName, obj);
     }
 
+
+    /**
+     * 销毁指定类名的单例对象。
+     *
+     * @param className 要销毁的类名
+     */
     public static void destroy(String className) {
         String beanName = beanName(className);
         listableBeanFactory.destroySingleton(beanName);
     }
 
+
+    /**
+     * 生成bean的名称
+     * @param className 类名
+     * @return bean的名称
+     */
     public static String beanName(String className) {
         String[] path = className.split("\\.");
         String beanName = path[path.length - 1];
         return Character.toLowerCase(beanName.charAt(0)) + beanName.substring(1);
     }
+
 
     @SuppressWarnings("unchecked")
     public static <T> T getBean(String name) {
