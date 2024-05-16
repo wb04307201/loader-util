7f5f110b5af4fd120775f0307fb5b2e8051799e3
wubo <wubo_aaa@163.com>
2024-05-16 22:02:51 +0800

    修改注释

--- src/main/java/cn/wubo/loader/util/class_loader/JavaMemClass.java
+++ src/main/java/cn/wubo/loader/util/class_loader/JavaMemClass.java
@@ -22,22 +22,25 @@ public class JavaMemClass extends SimpleJavaFileObject {
     /**
      * 获取字节数组
      *
-     * @return 字节数组
+     * 该方法不需要接受任何参数，它将返回一个字节数组。
+     * 主要用于将内部缓存的字节信息转换为字节数组输出。
+     *
+     * @return byte[] 返回一个包含字节信息的数组
      */
     public byte[] getBytes() {
+        // 将内部存储的字节流转换为字节数组并返回
         return classByteArrayOutputStream.toByteArray();
     }
 
- 
     /**
      * 重写openOutputStream方法
+     * 这个方法重写了父类中的openOutputStream方法，目的是提供一个特定的输出流返回。
      *
-     * @return 返回classByteArrayOutputStream对象
+     * @return 返回classByteArrayOutputStream对象 - 这是一个OutputStream的实例，
+     *         用于允许外部写入数据到类的内部缓存中。
      */
     @Override
     public OutputStream openOutputStream() {
         return classByteArrayOutputStream;
     }
-
-    
 }
--- src/main/java/cn/wubo/loader/util/class_loader/JavaMemSource.java
+++ src/main/java/cn/wubo/loader/util/class_loader/JavaMemSource.java
@@ -21,15 +21,17 @@ public class JavaMemSource extends SimpleJavaFileObject{
 	}
 	
 	/**
-	 * 获取源代码的注释。
+	 * 此方法用于获取源代码的注释内容。
+	 * 它会返回源代码文件中的字符内容，包括注释部分。
 	 *
-	 * @param ignoreEncodingErrors 是否忽略编码错误。
-	 * @return 源代码的字符内容。
+	 * @param ignoreEncodingErrors 指定是否忽略编码错误。如果设置为true，
+	 *                             在读取源代码文件时将忽略任何编码错误，
+	 *                             否则遇到编码错误将抛出异常。
+	 * @return 返回源代码的字符序列，包括代码中的所有注释。
 	 */
 	@Override
 	public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+	    // 直接返回存储的源代码字符序列
 	    return javaSourceCode;
 	}
-
-
 }
